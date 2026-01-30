9e0d273398c6f99195f79425c5a5601ec6e8edaa
Ashwani kumar yadav <ashwaniyadav0091@gmail.com>
2026-01-30 06:28:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+build command = npm run build
+Publish directory = dist
 
 export default defineConfig(({ mode }) => {
   let build: UserConfig['build'] = {}
